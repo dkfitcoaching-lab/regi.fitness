87e447889f36a4cfc58797dda89818840d749687
dkfitcoaching-lab <dkfitcoaching@gmail.com>
2026-02-27 20:37:53 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Self-clearing service worker - clears all caches and passes through all requests
+self.addEventListener(‘install’, () => self.skipWaiting());
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+.then(() => self.clients.claim())
+);
+});
+self.addEventListener(‘fetch’, e => e.respondWith(fetch(e.request)));
